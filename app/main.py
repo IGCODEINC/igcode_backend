8c3d3282a2e54de412225c71dc981ce0164465de
Anderson Rodrigues <eusouanderson@outlook.com>
2024-11-07 18:28:54 -0300

    Correction | Doc

--- app/main.py
+++ app/main.py
@@ -4,11 +4,15 @@ from app.crud import create_user, authenticate_user
 from app.database import get_database
 from pymongo.collection import Collection
 
-app = FastAPI()
+app = FastAPI(
+    title="API IGCODE",
+    description="API para gerenciamento de usuários",
+    version="1.0.0"  
+)
+
+app.name = "API IGCODE"
 
 
-
-# Dependência para obter a coleção de usuários
 def get_user_collection(db=Depends(get_database)) -> Collection:
     return db["users"]
 
@@ -17,11 +21,11 @@ def get_user_collection(db=Depends(get_database)) -> Collection:
 def read_root():
     return {"message" : "Servidor inicializado com sucesso!"}
 
-@app.post("/register/", response_model=UserResponse)
+@app.post("v1/register/", response_model=UserResponse)
 def register(user: UserCreate, collection: Collection = Depends(get_user_collection)):
     return create_user(collection, user)
 
 
-@app.post("/login/")
+@app.post("v1/login/")
 def login(user_login: UserLogin, collection: Collection = Depends(get_user_collection)):
     return authenticate_user(collection, user_login)
